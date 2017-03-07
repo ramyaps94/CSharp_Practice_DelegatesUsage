80b997a363f5def914aea0450b360e1f7b559a0e
Ramya P S <ramya.sudarshan1994@gmail.com>
2017-03-07 20:59:04 +0530

    Using Delegates:Promote Employees based on Salary and Experience

--- DelegatePractice2/DelegatePractice2/Program.cs
+++ DelegatePractice2/DelegatePractice2/Program.cs
@@ -6,9 +6,10 @@ using System.Threading.Tasks;
 
 namespace DelegatePractice2
 {
-    class Program
+    public delegate bool IsPromotabaleDelegate(Employee emp);
+    public class Program
     {
-        
+
         static void Main(string[] args)
         {
             List<Employee> empList = new List<Employee>();
@@ -17,9 +18,31 @@ namespace DelegatePractice2
             empList.Add(new Employee() { EMPId = 103, Name = "Jaanu", Experience = 5, Salary = 30000 });
             empList.Add(new Employee() { EMPId = 104, Name = "Jenny", Experience = 7, Salary = 60000 });
             empList.Add(new Employee() { EMPId = 105, Name = "Jeni", Experience = 3, Salary = 50000 });
-            Employee.PromoteEmployees(empList);
+            IsPromotabaleDelegate isPromotable = new IsPromotabaleDelegate(PromoteBasedOnExperience);
+            IsPromotabaleDelegate isPromotable1 = new IsPromotabaleDelegate(PromoteBasedOnSalary);
+            Employee.PromoteEmployees(empList, isPromotable);
+            Console.WriteLine("--------------------------");
+            Employee.PromoteEmployees(empList, isPromotable1);
+            Console.ReadLine();
+        }
+
+        public static bool PromoteBasedOnExperience(Employee emp)
+        {
+            if (emp.Experience > 5)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool PromoteBasedOnSalary(Employee emp)
+        {
+            if (emp.Salary > 40000)
+                return true;
+            else
+                return false;
         }
     }
+
 
     public class Employee
     {
@@ -28,12 +51,12 @@ namespace DelegatePractice2
         public double Salary { get; set; }
         public int Experience { get; set; }
 
-        public static void PromoteEmployees(List<Employee> employees)
+        public static void PromoteEmployees(List<Employee> employees, IsPromotabaleDelegate isEligibleToPromote)
         {
             foreach (Employee emp in employees)
             {
-                if(emp.Experience > 5)
-                    Console.WriteLine("Congratualations {0}!! You have been promoted!!",emp.Name);
+                if (isEligibleToPromote(emp))
+                    Console.WriteLine("Congratualations {0}!! You have been promoted!!", emp.Name);
             }
         }
     }
